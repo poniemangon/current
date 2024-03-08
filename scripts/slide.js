8ad3c2dda01c1b44c87a4a963eb353fcda7946bc
gon_14 <poniemangon@gmail.com>
2024-03-08 13:06:02 -0300

    primera version

---
+++ scripts/slide.js
@@ -0,0 +1,44 @@
+$('.carousel').slick({
+  arrows: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  responsive: [{
+      breakpoint: 1200,
+      settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: false,
+      }
+  },
+  {
+      breakpoint: 800,
+      settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: false,
+
+      }
+  }]
+});
+
+$('.carousel2').slick({
+    arrows: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    responsive: [
+    {
+        breakpoint: 1000,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: false,
+  
+        }
+    }]
+  });
+  
+
